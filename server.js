1c5b182d93f031e10ddb0c8f3037e1c1aeadc8dd
voolocaru catadenis <volocaru.catalin17@gmail.com>
2022-02-18 13:37:13 +0000

    changes in package.json

--- server.js
+++ server.js
@@ -1,16 +1,9 @@
-function requireHTTPS(req, res, next) {
-  if (!req.secure && req.get('x-forwarded-proto') !== 'https') {
-    return res.redirect('https://' + req.get('host') + req.url);
-  }
-  next();
-}
 const express = require('express');
+const path = require('path');
 const app = express();
-app.use(requireHTTPS);
-app.use(express.static('./dist/student-response'));
-
-app.get('/*', function(req, res) {
-  res.sendFile('index.html', { root: 'dist/student-response/'}
-);
+app.use(express.static(__dirname + '/dist/student-response'));
+app.get('/*', function (req, res) {
+  res.sendFile(path.join(__dirname +
+    '/dist/student-response/index.html'));
 });
 app.listen(process.env.PORT || 8080);
