4458e7bc7b4c27395dc8dc836eb2c3712b89d934
voolocaru catadenis <volocaru.catalin17@gmail.com>
2022-02-18 13:43:06 +0000

    changes in package.json

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-app.use(express.static(__dirname + '/dist/student-response'));
+app.use(express.static(__dirname + '/dist/student-response>'));
 app.get('/*', function (req, res) {
   res.sendFile(path.join(__dirname +
-    '/dist/student-response/index.html'));
+    '/dist/<student-response>/index.html'));
 });
 app.listen(process.env.PORT || 8080);
