42c7649c07a8f28f0d55c2e9ac32a6c16900b444
voolocaru catadenis <volocaru.catalin17@gmail.com>
2022-02-24 18:23:24 +0000

    Cors fixing

--- src/app/student.service.ts
+++ src/app/student.service.ts
@@ -13,23 +13,23 @@ export class StudentService {
   constructor( private http : HttpClient) { }
 
   public getAllStudents(): Observable<Student[]> {
-    return this.http.get<Student[]>('https://student-report-frontend-app.herokuapp.com/students/all');
+    return this.http.get<Student[]>('https://student-report-app.herokuapp.com/students/all');
   }
 
   public  createStudent(student: Student): Observable<Student> {
-    return this.http.post<Student>('https://student-report-frontend-app.herokuapp.com/students/add', student);
+    return this.http.post<Student>('https://student-report-app.herokuapp.com/students/add', student);
   }
 
   public getStudentById(id: number): Observable<Student> {
-    return this.http.get<Student>('https://student-report-frontend-app.herokuapp.com/find/${id}');
+    return this.http.get<Student>('https://student-report-app.herokuapp.com/find/${id}');
   }
 
   public updateStudent(student: Student): Observable<Student> {
-    return this.http.put<Student>('https://student-report-frontend-app.herokuapp.com/update',student);
+    return this.http.put<Student>('https://student-report-app.herokuapp.com/update',student);
   }
 
   public deleteStudent(id: number): Observable<void> {
-    return this.http.delete<void>('https://student-report-frontend-app.herokuapp.com/delete/${id}');
+    return this.http.delete<void>('https://student-report-app.herokuapp.com/delete/${id}');
   }
 
 }
